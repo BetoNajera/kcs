dc69225c41a8c4aa7beadd0e349025c3bb105724
BetoNajera <betonajera@formaseideas.net>
2018-10-19 00:07:33 -0500

    Animación logo casi lista

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -27,3 +27,33 @@ let fecha = new Date(),
 	];
 
 	document.getElementById('fecha').innerHTML = dias[fecha.getDay()] + ', ' + fecha.getDate() + ' de ' + meses[fecha.getMonth()] + ' de ' + fecha.getFullYear();
+
+
+// Menu responsive
+$(document).ready(main_menu);
+
+function main_menu() {
+	var marcador = 0;
+	if ($(window).width() < 769) {
+		$('.bt-menu').on('click', function () {
+			if (marcador == 1) {
+				$('.main-menu').animate({
+					top: '39'
+				});
+				$('.bt-menu').addClass('bt-menu2');
+
+				marcador = 0;
+			} else {
+				marcador = 1;
+				$('.main-menu').animate({
+					top: '-280'
+				});
+				$('.bt-menu').removeClass('bt-menu2');
+			}
+		});
+	} else {
+		$('.main-menu').css({
+			top: '0'
+		});
+	}
+}
